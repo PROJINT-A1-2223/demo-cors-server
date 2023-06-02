47ab4206cce275922fe696c4f9cf7e4c2bf45c16
Francois Roland <froland@froland.be>
2023-06-02 15:14:22 +0200

    Generated project structure.

---
+++ src/main/java/be/eafcuccle/projint/democorsserver/DemoCorsServerApplication.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.democorsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCorsServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoCorsServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/be/eafcuccle/projint/democorsserver/DemoCorsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.democorsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCorsServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
